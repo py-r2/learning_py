b17fe9e84d0fb5497cc1d925db2e42522404a99e
py-r2 <romeo.sistech.ca>
2018-03-30 21:28:19 -0600

    working on loading articles to the database

--- Projects/proj7_portfolio/app.py
+++ Projects/proj7_portfolio/app.py
@@ -1,17 +1,26 @@
-from flask import Flask, render_template, url_for
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, url_for, request
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLAlCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres123@localhost/articles'
 db = SQLAlchemy(app)
-app.config['SQLAlchemy_DATABASE_URI'] = 'postgresql://postgres:postgres123@localhost/articles_db'
 
-class Article(db.Model):
+class loadArticle(db.Model):
+    __tablename__="data"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(15), unique=True)
-    password = db.Column(db.String(80))
+    password = db.Column(db.String(20))
     email = db.Column(db.String(50), unique=True)
-    article = db.Column(db.String())
+    title = db.Column(db.String(30), unique=True)
+    article = db.Column(db.String(120))
 #    created_at = db.Column(db.DateTime, default=datetime.now())
+
+# def __init__(self,username,password,email,title,article):
+#     self.username=username
+#     self.password=password
+#     self.email=email
+#     self.title=title
+#     self.article=article
 
 @app.route('/')
 def home():
@@ -33,6 +42,18 @@ def about():
 def loadarticle():
     return render_template('load-article.html')
 
+@app.route('/write', methods=['POST'])
+def write_to_database():
+    if request.method == 'POST':
+        title = request.form["article_title"]
+        article = request.form["article_body"]
+        if (db.session.query(loadArticle).filter(loadArticle.title==title).count == 0):
+            load_article = loadArticle(title,article)
+            db.session.add(load_article)
+            db.commit()
+            return render_template("articles.html")
+        pass
+    return render_template('write-article.html')
 
 if __name__=='__main__':
     app.run(debug=True)
